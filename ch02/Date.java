b095872dc0e133d56e76834f14705e86200d4dd7
jcbra <jcbradshaw88@gmail.com>
2018-01-18 15:05:18 -0600

    Date Formats

---
+++ ch02/Date.java
@@ -0,0 +1,38 @@
+public class Date
+
+{
+
+
+    public static void main(String[] args)
+
+    {
+
+        System.out.println("Todays Date");
+
+        String day = "Thursday, ";
+        String month = "January";
+        int date = 18 ;
+        int year = 2018;
+
+        System.out.println("American Format:");
+        System.out.print(day);
+        System.out.print(" ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.println(year);
+
+        System.out.println("European Format:");
+        System.out.print(day);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(" ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.println(year);
+
+    }
+
+
+}
