4c5ce5093905df4cb26d66ea647e50532dcf89f0
jcbra <jcbradshaw88@gmail.com>
2018-01-19 16:44:19 -0600

    IntExtremes, Exercise 2.4

---
+++ ch02/IntExtremes.java
@@ -0,0 +1,23 @@
+public class IntExtremes
+{
+    public static void main(String[] args)
+
+    {
+        int positiveInt;
+        positiveInt = 2147483647;
+
+        System.out.println("Positive Int is: " + positiveInt);
+
+        System.out.print("Positive Int Increase by 1: ");
+        System.out.println(positiveInt + 1);
+
+        int negativeInt;
+        negativeInt = -2147483648;
+
+        System.out.print("Negative Int Decrease by 1: ");
+        System.out.println(negativeInt - 1);
+
+
+
+    }
+}
