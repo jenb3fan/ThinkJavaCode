f77cc9fe3599dbb3751283edb0a2691afd133bdb
jcbra <jcbradshaw88@gmail.com>
2018-01-21 08:29:01 -0600

    IntByZero Exercise 2.5

---
+++ ch02/IntByZero.java
@@ -0,0 +1,22 @@
+public class IntByZero
+{
+
+    public static void main(String[] args)
+
+    {
+        int a = 42;
+        int b = 0;
+        int result = (b / a);
+
+
+        System.out.print("Result: " +result);
+
+
+
+
+
+
+
+
+    }
+}
