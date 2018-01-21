027835a1bcb5ee42dd4bf93b10b58e1af3c81e79
jcbra <jcbradshaw88@gmail.com>
2018-01-21 08:52:19 -0600

    DoubleByZero Exercise 2.6

---
+++ ch02/DoubleByZero.java
@@ -0,0 +1,25 @@
+public class DoubleByZero
+{
+    public static void main(String[] args)
+
+
+    {
+        double a = 42.0;
+        double b = 0.0;
+        double result = a / b;
+
+        System.out.println("Result is; " + result);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
