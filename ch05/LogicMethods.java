01bb0d3718da159130400ce31d252d81c4569f19
jcbra <jcbradshaw88@gmail.com>
2018-01-22 14:05:34 -0600

    LogicMethods Exercise 5.C

--- ch05/LogicMethods.java
+++ ch05/LogicMethods.java
@@ -2,34 +2,22 @@ public class LogicMethods
 {
     public static void main(String[] args)
 
+
     {
         System.out.println("Start of Program");
-        printIsLargeOrSmall(1);
-        printIsLargeOrSmall(99);
-        printIsLargeOrSmall(100);
-        printIsLargeOrSmall(200);
+        printLargest(1);
+        printLargest(2);
     }
 
-    private static void printIsLargeOrSmall(int number)
+
+    private static void printLargest(int number)
+
     {
-        System.out.println("printIsLargeOrSmall");
-        System.out.println("Number is: " + number);
-
-
-        if(number >= 99)
+        if (number > 1)
         {
-            System.out.println("The number is large " + number );
-        }
-        else if (number < 10)
-        {
-            System.out.println("The number is small " + number);
+            System.out.println("The Largest Number is: " + number);
         }
 
-        System.out.println();
     }
-
-
-
-
 
 }
